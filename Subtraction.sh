0fd74977a041ebb73ae9831a5cc3331758322c36
SATYA PRANEETH KOUSHIK SRIPADA <praneethk9912@gmail.com>
2025-11-06 20:48:51 +0530

    Create Subtraction.sh

---
+++ Subtraction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#Difference of two numbers
+
+echo "Subtaction:"
+
+read -p " Take the input Values:" Value1 Value2
+
+Value1=$Value1
+Value2=$Value2
+
+Result=$(($Value1 - $Value2))
+
+echo " The difference of the two numbers is :" $Result
+
+
+
